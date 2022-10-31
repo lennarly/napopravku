ac6949a17fd77384ff648b8f1df3c5cc39a4d1be
lennarly <yourlenar@gmail.com>
2022-10-31 21:20:18 +0500

    Базовая информация о пользователе

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+
+class UserController extends Controller
+{
+    /**
+     * Get information about an authorized user.
+     * @return JsonResponse
+     */
+    public function info()
+    {
+        return response()->json(
+            Auth::user()
+        );
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,30 +1,19 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\AuthController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
-/*
- * Auth
- */
+// Auth
 
 Route::prefix('auth')->group(function () {
     Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+});
+
+// User
+
+Route::middleware('auth:sanctum')->prefix('user')->group(function () {
+    Route::get('/info', [UserController::class, 'info']);
 });
